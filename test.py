9d470678182fb9b194db9e60002345cefebea0d5
Dharmendra Negi <dharmendra.s.negi96@gmail.com>
2024-01-28 22:41:08 +0530

    updated esbuild

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("https://dslsi9j2pb.execute-api.ap-south-1.amazonaws.com/dev/hello")
+        self.client.get("/hello")
